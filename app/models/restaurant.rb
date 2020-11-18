96be7b79a931cda0b69cbff0032de6f6f53f34ca
Marc Neuschwander <marc.neusch@icloud.com>
2020-11-18 15:19:18 +0100

    Rstaurant Routes, Controller, view done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  CATEGORY = ['chinese', 'italian', 'french', 'japanese', 'belgian']
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: CATEGORY }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+Restaurant.create!(
+  name: 'Mario',
+  address: 'lyon',
+  phone_number: '0603629346',
+  category: 'italian'
+)
+
+Restaurant.create!(
+  name: 'welsh',
+  address: 'Lille',
+  phone_number: '0608792346',
+  category: 'belgian'
+)
+
+Restaurant.create!(
+  name: 'la Bijouterie',
+  address: 'Lyon',
+  phone_number: '0645692346',
+  category: 'french'
+)
+
+Restaurant.create!(
+  name: 'kenbo',
+  address: 'Paris',
+  phone_number: '0603692146',
+  category: 'chinese'
+)
+
+Restaurant.create!(
+  name: 'Naruto',
+  address: 'Konoha',
+  phone_number: '0603692766',
+  category: 'japanese'
+)
+puts "it's done"
